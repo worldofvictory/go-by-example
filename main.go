4388d3733a667d4824558b803c38a3401760b53e
worldofvictory <133643234+worldofvictory@users.noreply.github.com>
2025-07-17 14:57:25 +0200

    maps. slice

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 	"time"
 )
 
+func vals() (int, int) {
+	return 3, 7
+}
+
 func main() {
 	i := 3
 	fmt.Print("Write ", i, " as ")
@@ -32,4 +36,30 @@ func main() {
 	fmt.Println("get:", a[0])
 
 	fmt.Println("len:", len(a))
+	fmt.Println("##########################")
+	//slice
+	var s []string
+	fmt.Println("uninit:", s, s == nil, len(s) == 0)
+	s = make([]string, 3)
+	fmt.Println("emp:", s, "len:", len(s), "cap:", cap(s))
+
+	s[1] = "b"
+	s[2] = "c"
+	fmt.Println("set:", s)
+	fmt.Println("get:", s[2])
+
+	fmt.Println("len:", len(s))
+	fmt.Println("##########################")
+	s = append(s, "d")
+	s = append(s, "e", "f")
+	s[5] = "a"
+	fmt.Println("get:", s[0])
+	fmt.Println("apd:", s)
+
+	n, l := vals()
+	fmt.Println(n)
+	fmt.Println(l)
+
+	_, c := vals()
+	fmt.Println(c)
 }
